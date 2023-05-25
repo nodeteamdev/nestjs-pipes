1ad9d1ac13d2a57e70b18ee5469914efc51dbc2c
Checha Valerii <chechavalera@gmail.com>
2023-05-25 11:08:31 +0300

    chore(version): bump version

--- test/helpers/parse-object-literal.spec.ts
+++ test/helpers/parse-object-literal.spec.ts
@@ -1,15 +1,31 @@
 import parseObjectLiteral from '../../src/helpers/parse-object-literal';
 
 describe('parseObjectLiteral', () => {
-  it('should parse a string like "a: 1, b: 2" to [["a", "1"], ["b", "2"]]', () => {
-    const obj = 'a: 1, b: 2';
-
-    const result = parseObjectLiteral(obj);
-
-    expect(result).toEqual([['a', '1'], ['b', '2']]);
+  it('should parse a string with one key-value pair', () => {
+    const input = 'a: 1';
+    const expectedOutput = [['a', '1']];
+    const actualOutput = parseObjectLiteral(input);
+    expect(actualOutput).toEqual(expectedOutput);
   });
 
-  it('should be defined', () => {
-    expect(parseObjectLiteral).toBeDefined();
+  it('should parse a string with multiple key-value pairs', () => {
+    const input = 'a: 1, b: 2, c: 3';
+    const expectedOutput = [['a', '1'], ['b', '2'], ['c', '3']];
+    const actualOutput = parseObjectLiteral(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+
+  it('should parse a string with a single-quoted value', () => {
+    const input = 'a: \'1\'';
+    const expectedOutput = [['a', '\'1\'']];
+    const actualOutput = parseObjectLiteral(input);
+    expect(actualOutput).toEqual(expectedOutput);
+  });
+
+  it('should parse a string with a double-quoted value', () => {
+    const input = 'a: "1"';
+    const expectedOutput = [['a', '"1"']];
+    const actualOutput = parseObjectLiteral(input);
+    expect(actualOutput).toEqual(expectedOutput);
   });
 });
