22f0fabb63cb9376f301e5366d6c13ea7de7fca0
koder-dev <odilovmuhamadkodir@gmail.com>
2024-03-26 20:18:06 +0200

    feat(bug): fix import statement of pipe in dist file.

--- dist/src/prisma/where.pipe.d.ts
+++ dist/src/prisma/where.pipe.d.ts
@@ -1,5 +1,5 @@
 import { PipeTransform } from '@nestjs/common';
-import { Pipes } from '../index';
+import { Pipes } from '../../../index';
 export default class WherePipe implements PipeTransform {
     transform(value: string): Pipes.Where | undefined;
 }
