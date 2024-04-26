4df7ec43412838c9ffedf6f1ed9f6e404b636269
Yusuf Dwi <dwcahyo23@gmail.com>
2024-04-26 11:37:49 +0700

    changes nested object  with is operator

--- dist/src/helpers/delimeted-string-object.js
+++ dist/src/helpers/delimeted-string-object.js
@@ -3,8 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function delimetedStringObject(n, v, d) {
     n = n.split(d || '.');
     n.reverse();
-    return n.reduce(function (res, it) {
-        return { [it]: res };
+    return n.reduce(function (res, it, c) {
+        if (c === 0)
+            return { [it]: res };
+        return { [it]: { is: res } };
     }, v);
 }
 exports.default = delimetedStringObject;
